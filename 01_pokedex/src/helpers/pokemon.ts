57ecb722696a80e8c3331293a0dd32b9b6f0e8bc
Marcelo Soares <soares.srp@gmail.com>
2023-02-01 23:39:07 -0300

    Refactor: refatorando função generatePokemonPromises

--- 01_pokedex/src/helpers/pokemon.ts
+++ 01_pokedex/src/helpers/pokemon.ts
@@ -5,20 +5,18 @@ const getPokemonURL = (id: number): string =>
   `https://pokeapi.co/api/v2/pokemon/${id}`
 
 export const generatePokemonPromises = () => {
-  return Array<number>(150)
-    .fill(150)
-    .map((_, index) =>
-      axios
-        .get<GetPokemonData>(getPokemonURL(index + 1))
-        .then(({ data }) => data)
-        .catch(error => {
-          if (axios.isAxiosError(error)) {
-            console.log('error message: ', error.message)
-            return error.message
-          } else {
-            console.log('unexpected error: ', error)
-            return 'An unexpected error occurred'
-          }
-        }),
-    )
+  return Array.from({ length: 150 }).map(async (_, index) => {
+    try {
+      const { data } = await axios.get<GetPokemonData>(getPokemonURL(index + 1))
+      return data
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message)
+        return error.message
+      } else {
+        console.log('unexpected error: ', error)
+        return 'An unexpected error occurred'
+      }
+    }
+  })
 }
